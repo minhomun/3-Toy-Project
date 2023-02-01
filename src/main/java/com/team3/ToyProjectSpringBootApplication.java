5f66ff1719fb7bb8fcd8cec708547559232161f3
장규 민 <alswkdrb1@naver.com>
2023-02-01 14:20:22 +0900

    init

---
+++ src/main/java/com/team3/ToyProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyProjectSpringBootApplication.class, args);
+    }
+
+}
